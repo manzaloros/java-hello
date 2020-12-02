bfbb817ea290c78ad916e0c45bfc88c6e66dc202
manzaloros <zacharymansellcello@gmail.com>
2020-12-01 20:27:19 -0600

    Add first commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    static int printInt(int input) {
+        return input;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Should be args : " + args[0]);
+    }
+}
